f5b4b5dc91fe7fdaa594c46998c9f09f579a6a4d
Deepu <deepujoshi552@gmail.com>
2026-06-01 21:14:34 +0530

    Remove dist and node_modules

--- dist/server/routes/statutoryReport.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const statutoryReportController_js_1 = require("../controllers/statutoryReportController.js");
-const auth_js_1 = require("../middleware/auth.js");
-const router = (0, express_1.Router)();
-router.get("/", auth_js_1.authMiddleware, statutoryReportController_js_1.getStatutoryReports);
-router.post("/generate", auth_js_1.authMiddleware, statutoryReportController_js_1.generateStatutoryReports);
-exports.default = router;
